06b028c8e65b667b2140d7b50074c5087fc9beaf
Ravi <ravinj2003@gmail.com>
2026-07-12 17:15:48 +0530

    traversal added

--- Bst.java
+++ Bst.java
@@ -7,6 +7,7 @@ class Bst{
             root = newNode;
             return;
         }
+
         Node temp = root;
         while(true){
             if(d>temp.data){
@@ -23,8 +24,36 @@ class Bst{
                 temp = temp.lNode;
             }
         }
-        
+    }
 
+    // inorder bst traversal
+    public void inOrder(Node n){
+        if(n == null){
+            return;
+        }
+        inOrder(n.lNode);
+        System.out.println(n.data);
+        inOrder(n.rNode);
+    }
+
+    // pre order bst traversal 
+    public void preOrder(Node n){
+        if(n == null){
+            return;
+        }
+        System.out.println(n.data);
+        preOrder(n.lNode);
+        preOrder(n.rNode);
+    }
+
+    // postorder bst traversal
+    public void postOrder(Node n){
+        if(n == null){
+            return;
+        }
+        postOrder(n.lNode);
+        postOrder(n.rNode);
+        System.out.println(n.data);
     }
 
     public static void main(String[] args) {
@@ -34,6 +63,16 @@ class Bst{
         b.add(12);
         b.add(5);
         b.add(69);
-
+        // b.add(8);
+        // b.add(111);
+        // b.add(1);
+        // b.add(6);
+        Node temp = b.root;
+        System.out.println("INorder tree traversal ");
+        b.inOrder(temp);
+        System.out.println("pre order tree traversal ");
+        b.preOrder(temp);
+        System.out.println("post order bst traversal");
+        b.postOrder(temp);
     }
 }
